1c2a12011ab8834ee4d6db325136c90ff67dcb9b
Alex Normand <anormnd@gmail.com>
2014-08-19 05:13:50 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,16 @@
 var fn = function(a) {
   var hello = 'blabla';
 
-
   return function(b) {
+
+    var f = function(c) {
+      return function(d) {
+        var aaa = 45;
+
+        return a + b + c + d;
+      }
+    };
+
     return a + b;
   }
 };
